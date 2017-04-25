b469bd5cca7aeaa77706e68296c742f5c7cf2049
Dana L. Weinstein <danalweinstein@gmail.com>
2017-04-25 09:58:05 -0500

    Updated READ function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get("photos/:the_id_number", {
+  :controller => "photos",
+  :action => "show"
+  })
+
+get("photos", {
+  :controller => "photos",
+  :action => "index"
+})
 end
